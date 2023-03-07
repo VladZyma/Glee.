d4245373fa713842de309dcb2d4584482ce5928e
VladZyma <vladzima13@gmail.com>
2023-03-07 12:07:18 +0200

    added product-details page

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,23 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 3000,
   });
+  $('.details__slide-thumb').slick({
+    asNavFor: '.details__slide-big',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false,
+  });
+  $('.details__slide-big').slick({
+    asNavFor: '.details__slide-thumb',
+    arrows: false,
+    draggable: false,
+  });
+
+  $('.related__items').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  });
 
   $('.filter-price__input').ionRangeSlider({
     type: 'double',
@@ -32,6 +49,8 @@ $(function () {
     readOnly: true,
   });
 
+ 
+
   $('.product-content__filter-btn').on('click', function () {
     $('.product-content__filter-btn').removeClass('product-content__filter-btn--active');
     $(this).addClass('product-content__filter-btn--active');
@@ -43,7 +62,17 @@ $(function () {
     $('.products__item').addClass('products__item--list');
   })
 
-  $('.product-content__filter-sort').styler();
+  $('.product-content__filter-sort, .details__input').styler();
+
+  $('.product-tabs__top-item').on('click', function (e) {
+    e.preventDefault();
+
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+  })
 
  
   const mixerOne = mixitup('.products__items',{
